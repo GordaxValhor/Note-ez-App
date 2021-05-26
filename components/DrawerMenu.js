a81f78ecd19ed66b7eca5ed004cfdbba75bc4c96
GordaxValhor <calin.ianos21@gmail.com>
2021-05-26 21:00:35 +0300

    primul pas pentru modal

--- components/DrawerMenu.js
+++ components/DrawerMenu.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import { View, Text, StyleSheet,Image,ScrollView } from 'react-native'
+import React,{useState} from 'react'
+import { View, Text, StyleSheet,Image,ScrollView,TouchableOpacity,Modal,TextInput } from 'react-native'
 
 import {
     DrawerContentScrollView,
@@ -10,8 +10,35 @@ import {globalStyles} from '../assets/globalStyles/globalStyles'
 
 
 const DrawerMenu = () => {
+
+
+
+
+    const [modalVisible, setModalVisible] = useState(false);
     return (
         <View style={[globalStyles.container,styles.container]}>
+                <Modal
+                    animationType="slide"
+                    transparent={true}
+                    visible={modalVisible}
+                    onRequestClose={() => {
+                        setModalVisible(!modalVisible);
+                    }}>
+                    <View style={[styles.modalStyle,{top:'35%',left:'25%'}]}>
+                        <TouchableOpacity onPress={()=> setModalVisible(!modalVisible)}>
+                            <View style={{marginLeft:'90%'}}>
+                            <Text style={[globalStyles.text]}>X</Text>
+                            </View>
+                        </TouchableOpacity>
+                            <View style={{margin:15,padding:5,alignItems:'center'}}>
+                                {/* <Text style={globalStyles.text}>Adauga un label nou</Text> */}
+                                <TextInput placeholderTextColor="#fff4" style={globalStyles.text} placeholder='Adauga un label nou'></TextInput>
+                            </View>
+                        <TouchableOpacity>
+                            <Text style={globalStyles.text}>Add</Text>
+                        </TouchableOpacity>
+                    </View>
+                </Modal>
                 <View style={{borderBottomWidth:1,borderColor:'white',width:'100%',padding:10}}>
                         <Image
                         style={{resizeMode:'contain',height:50,width:'100%'}}
@@ -34,8 +61,13 @@ const DrawerMenu = () => {
                                 <Text style={globalStyles.text}>Echipa 2</Text>
                             </View>
                         </View>
-                        <View style={{marginTop:20,marginLeft:9}}>
-                            <Text style={styles.titlu}>Labels</Text>
+                        <View style={{marginTop:20,marginLeft:9,width:250,}}>
+                            <View style={{flex:1,flexDirection:'row',justifyContent:'space-between',}}>
+                                <Text style={styles.titlu}>Labels</Text>
+                                <TouchableOpacity onPress={()=> setModalVisible(!modalVisible)}>
+                                    <Text style={styles.titlu}>Add</Text>
+                                </TouchableOpacity>
+                            </View>
                                 <View style={{marginLeft:20,marginTop:5,}}>
                                     <Text style={globalStyles.text}>Label 1</Text>
                                     <Text style={globalStyles.text}>Label 2</Text>
@@ -57,9 +89,9 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         flexDirection: 'column',
-        justifyContent: 'flex-start',
-        justifyContent: 'flex-start',
-        alignItems:'flex-start',
+        //justifyContent: 'flex-start',
+        //justifyContent: 'flex-start',
+        //alignItems:'flex-start',
         backgroundColor: '#0a0a0a',
         borderRightWidth: 1,
         borderColor: 'white',
@@ -68,5 +100,15 @@ const styles = StyleSheet.create({
     titlu: {
         fontSize: 18,
         color: 'white',
+    },
+    modalStyle:{
+        width:270,
+        minHeight:120,
+        borderWidth: 2,
+        borderColor:'#8C8C8C',
+        backgroundColor: '#0a0a0a',
+        margin:5,
+        padding: 12,
+        borderRadius: 5,
     }
 })
